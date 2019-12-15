a780f56cf5ef217d7aadc4c1f121ecd51cbfef84
Anish Aggarwal <anish17122000@gmail.com>
2019-12-16 00:34:07 +0530

    test

---
+++ build/precache-manifest.82107715bf6079e6ca55cc1f4cb62cf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c65d69329699c4592da24cb4086a72",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "13819a67c9db7bbbeccd",
+    "url": "/react-weather/static/css/main.4cf6bfd3.chunk.css"
+  },
+  {
+    "revision": "c21131d1b07d3e5cabe2",
+    "url": "/react-weather/static/js/2.1b6cec65.chunk.js"
+  },
+  {
+    "revision": "13819a67c9db7bbbeccd",
+    "url": "/react-weather/static/js/main.b01dba6b.chunk.js"
+  },
+  {
+    "revision": "838bc56962ea251bf0a5",
+    "url": "/react-weather/static/js/runtime-main.77074d45.js"
+  }
+]);
